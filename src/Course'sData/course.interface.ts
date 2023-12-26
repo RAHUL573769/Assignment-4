a49e225141855568e94858e59b56838648014cf5
Rahul Rudra <rahulrudra665@gmail.com>
2023-12-26 15:55:26 +0600

    changes made

---
+++ src/Course'sData/course.interface.ts
@@ -0,0 +1,24 @@
+import { Types } from "mongoose";
+
+export interface ITags {
+  name: string;
+  isDeleted: boolean;
+}
+
+export type IDetails = {
+  level: string;
+  description: string;
+};
+
+export interface ICourse {
+  title: string;
+  instructor: string;
+  categoryId: Types.ObjectId;
+  price: number;
+  tags: ITags[];
+  startDate: string;
+  endDate: string;
+  language: string;
+  provider: string;
+  details: IDetails;
+}
